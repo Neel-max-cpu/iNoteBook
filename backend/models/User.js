268953e24abd7f739894d7651f6db05e5ccd2995
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-06-05 11:04:26 +0530

    improving the authentication :memo:

--- backend/models/User.js
+++ backend/models/User.js
@@ -27,5 +27,4 @@ const UserSchema = new Schema({
 // module.exports = mongoose.model('user', UserSchema);
 
 const User = mongoose.model('user', UserSchema);
-User.createIndexes();
 module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,27 +4,45 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
 
-// CREATE A User using : POST "/api/auth". Doesn't require Authentication
+// CREATE A User using : POST "/api/auth/createuser". Doesn't require Authentication/ No login required
 
 
-router.post('/', [
+router.post('/createuser', [
     body('name', 'Enter a valid Name').isLength({min:3}),
     body('email', 'Enter a valid Email').isEmail(),
     body('password', 'Password must have a minimum of 5 characters').isLength({min:5}),
-] ,(req, res)=>{
+] ,async (req, res)=>{
+    // if there are errors return BAD requests and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-    }).then(user=>res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error: 'Email already exists!'})})
-    
-    // res.send(req.body);
+
+    // check whether the user with this email exists already
+    try {    
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error: "Sorry a user with this email already exists"});
+        }
+        // create a new user
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+        })
+        // .then(user=>res.json(user))
+        // .catch(err=>{console.log(err)
+        // res.json({error: 'Email already exists!'})})
+        
+        // res.send(req.body);
+
+        // send this or ---
+        // res.json({"Success": "user has been created!"})
+        res.json({user})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some Error occured");
+    }
 })
 
 module.exports = router;
