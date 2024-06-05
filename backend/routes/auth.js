c35fa495b9bfaee44b9946897c589d4aa76ff70e
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-06-05 15:44:38 +0530

    making jwt token :memo:

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); 
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'nEELisagoodb$oy';
 
 
 // CREATE A User using : POST "/api/auth/createuser". Doesn't require Authentication/ No login required
-
-
 router.post('/createuser', [
     body('name', 'Enter a valid Name').isLength({min:3}),
     body('email', 'Enter a valid Email').isEmail(),
@@ -18,16 +20,18 @@ router.post('/createuser', [
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // check whether the user with this email exists already
     try {    
+        // check whether the user with this email exists already
         let user = await User.findOne({email: req.body.email});
         if(user){
             return res.status(400).json({error: "Sorry a user with this email already exists"});
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         // create a new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
         })
         // .then(user=>res.json(user))
@@ -38,7 +42,19 @@ router.post('/createuser', [
 
         // send this or ---
         // res.json({"Success": "user has been created!"})
-        res.json({user})
+        // or this---
+        // res.json({user})
+
+
+        // send now this , jwt token
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken});
+
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Some Error occured");
