c7e562d99c6bed5523b8def3535eb01859fe97ec
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-06-07 16:10:11 +0530

    adding navbar :memo:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-
 import './App.css';
+import React, { useState, useEffect} from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+import LoadingBar from 'react-top-loading-bar'
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  // for loading bar --
+  const [progress, setProgress] = useState(0);
+
+  const applyBodyStyle = () => {
+    // document.body.style.backgroundColor = this.state.mode === 'light' ? 'white' : '#403d3d';
+    document.body.style.backgroundColor = mode === 'light' ? 'white' : '#403d3d';
+    document.body.style.color = mode === 'light' ? 'rgb(0 0 0 / 85%)' : 'white';
+  };
+
+  useEffect(() => {
+    applyBodyStyle();
+    // eslint-disable-next-line
+  }, [mode]);
+
+  const toggleMode = () => {
+    setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+
+
   return (
-    <div className="App">
-    <h1>HEHE I AM NEEL!</h1>
-    </div>
+    <BrowserRouter>
+      <>
+        <Navbar mode={mode} toggleMode={toggleMode}/>
+        <LoadingBar
+          color={`${mode==='light'? '#f11946' : 'yellow'}`}
+          // progress={this.state.progress}
+          progress={progress}
+          height={3}
+          // onLoaderFinished={() => setProgress(0)}
+        />
+        <Routes>
+          <Route exact path="/" element={<Home setProgress={setProgress} mode={mode}/>}></Route>
+          <Route exact path="/about" element={<About setProgress={setProgress} mode={mode}/>}></Route>
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Link} from 'react-router-dom';
+
+import './Navbar.css';
+
+const Navbar = (props) => {
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleItemClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    const { mode, toggleMode } = props;
+
+    return (
+        <div>
+              <nav className={`navbar fixed-top navbar-expand-lg ${mode === 'light' ? 'navbar-light' : 'navbar-dark'}`}>
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">iNoteBook</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item" onClick={() => handleItemClick(0)}>
+                                <Link className={`nav-link ${activeIndex === 0 ? 'active' : ''}`} aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item" onClick={() => handleItemClick(1)}>
+                              <Link className={`nav-link ${activeIndex === 1 ? 'active' : ''}`} to="/about">About</Link>
+                            </li>
+                        </ul>
+                        <div className={`form-check form-switch text-${mode === 'light' ? 'dark' : 'light'}`}>
+                          <input className="form-check-input" type="checkbox" role="switch" onClick={toggleMode} id="flexSwitchCheckDefault"/>
+                          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+                            {mode === 'light' ? 'Enable Dark Mode' : 'Enable Light Mode'}
+                          </label>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
