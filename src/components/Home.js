235f942d1c38905a7cf31198e99cf277e5123432
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-06-09 13:08:30 +0530

    hardcoding notes :memo:

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import noteContext from "../context/notes/noteContext.js"
 import './Home.css';
 
 const Home = () => {
+  const context = useContext(noteContext);
+  const {notes, setNotes} = context;
+
   return (
     <div className='custom'>
       <div className="container my-4">
@@ -26,6 +30,9 @@ const Home = () => {
 
       <div className="container my-4">
         <h2>Your Notes</h2>
+        {notes.map((note)=>{
+          return note.title;
+        })}
       </div>
       
       <div className="container my-4">
--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -2,10 +2,32 @@ import NoteContext from './noteContext';
 import { useState } from 'react';
 
 const NoteState = (props)=>{
+    const notesInitial = [
+        {
+          "_id": "666557e5f73b32468e64e4ac",
+          "user": "6660423e01264f998d2a1b88",
+          "title": "hehe i know",
+          "description": "i know ok hehe, and you can't do anything about it",
+          "tag": "Personal",
+          "date": "2024-06-09T07:21:09.259Z",
+          "__v": 0
+        },
+        {
+          "_id": "666557ecf73b32468e64e4ae",
+          "user": "6660423e01264f998d2a1b88",
+          "title": "hehe i know2",
+          "description": "i know ok hehe2, and you can't do anything about it",
+          "tag": "Personal",
+          "date": "2024-06-09T07:21:16.270Z",
+          "__v": 0
+        }
+      ]
 
+      const [notes, setNotes] = useState(notesInitial);
+      
     
     return(
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
