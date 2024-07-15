ec2ceebf5d98669e7ff92e0ad36dcf2f6e71b855
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-15 21:13:20 +0530

    cors new :memo:

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors');
+const allowCors = require('./corsMiddleware'); // Import the custom CORS middleware
 
 connectToMongo();
 
@@ -11,19 +12,19 @@ const port = 5000
 
 // write to so that it doesn't give error while sending request
 // CORS options
-const corsOptions = {
-  origin: ['https://i-note-book-frontend-ashen.vercel.app/login'], // Your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'],
-  credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
-  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: ['https://i-note-book-frontend-ashen.vercel.app/login'], // Your frontend domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'],
+//   credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
+//   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 // Use CORS middleware with options
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // Explicitly handle preflight requests
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 // app.use(cors(
 //   {
@@ -34,6 +35,12 @@ app.options('*', cors(corsOptions));
 //     optionsSuccessStatus: 200,
 //   }
 // ));
+
+
+// use the custom cors this time ---
+app.use(allowCors((req, res, next) => {
+  next();
+}));
 
 
 app.use(express.json())
