14be88b898a8f8b2124c2ad8f1e5cd2d22c7cb21
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-15 12:50:13 +0530

    making cors work :memo:

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,13 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-// write to so that it doesn't give errro while sending request
-app.use(cors(
-  {
-    origin: ["https://i-note-book-frontend-five.vercel.app/login"],
-    methods: ["POST", "GET", "PUT"],
-    credentials: true
-  }
-));
+// write to so that it doesn't give error while sending request
+app.use(cors({
+  origin: 'https://i-note-book-frontend-five.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json())
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
