f7ea8e94b83ac6a0c481dafc40b2662970bbc271
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-15 12:36:24 +0530

    changin cors :memo:

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,14 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-app.use(cors());
+// write to so that it doesn't give errro while sending request
+app.use(cors(
+  {
+    origin: ["https://i-note-book-frontend-five.vercel.app/login"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true
+  }
+));
 app.use(express.json())
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
