939d3f3a4f08b5332af109e454a8eda7a2ebecdd
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-15 14:30:57 +0530

    200 added :memo:

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,7 @@ app.use(cors(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    optionsSuccessStatus: 200 
   } 
 ));
 
