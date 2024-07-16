15f6456a97b4bb608bae518cf031673204730d1d
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-16 12:10:03 +0530

    changing mongo db :memo:

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 // for local -- testing 
-const mongoURI = "mongodb://localhost:27017/inotebook"
+// const mongoURI = "mongodb://localhost:27017/inotebook"
 
-// const mongoURI = "mongodb+srv://neelbhatta22:iNotebookpass@inotebook.56ofnwa.mongodb.net/"
+const mongoURI = "mongodb+srv://neelbhatta22:iNotebookpass@inotebook.56ofnwa.mongodb.net/"
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
