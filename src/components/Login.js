3aa671220f70882149aebdb26f85268d265e41dc
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-16 19:12:04 +0530

    change vercel link in frontend :memo:

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,7 +18,7 @@ const Login = (props) => {
     // const response = await fetch("http://localhost:5000/api/auth/login", {
 
     // for vercel
-    const response = await fetch("https://i-note-book-backend-eight.vercel.app/auth/login", {
+    const response = await fetch("https://i-note-book-backend-eight.vercel.app/api/auth/login", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
