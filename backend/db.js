0926b35ebcdc4e6c1ad6e96069d8282b4191d030
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-07-17 12:05:28 +0530

    images :memo:

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 // for local -- testing 
-// const mongoURI = "mongodb://localhost:27017/inotebook"
+const mongoURI = "mongodb://localhost:27017/inotebook"
 
-// online mongodb
-const mongoURI = "mongodb+srv://neelbhatta22:iNotebookpass@inotebook.56ofnwa.mongodb.net/inotebook"
+// online mongodb vercel
+// const mongoURI = "mongodb+srv://neelbhatta22:iNotebookpass@inotebook.56ofnwa.mongodb.net/inotebook"
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
